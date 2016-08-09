859aaad3d9030315d503a78a336e357e5802cdf1
Gunther Verhemeldonck <gunther.verhemeldonck@gfi.be>
2016-08-09 14:32:27 +0200

    other message

--- src/main/java/be/gfi/academy/service/BaseAcademyServiceImpl.java
+++ src/main/java/be/gfi/academy/service/BaseAcademyServiceImpl.java
@@ -16,7 +16,7 @@ public class BaseAcademyServiceImpl implements AcademyService {
 	@Override
 	public String getGreeting() {
 		final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		return "Hello GUYS! Server time is currently set at " + sdf.format(getNow());
+		return "Hello GIRLS! Server time is currently set at " + sdf.format(getNow());
 	}
 
 	protected Date getNow() {
